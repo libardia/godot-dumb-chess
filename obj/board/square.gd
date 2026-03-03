0cec7b802e2a5d74cf75e078be81c606cd25bcb5
Anthony Libardi <libardi42@gmail.com>
2026-03-03 14:14:17 -0500

    hover and select

--- obj/board/square.gd
+++ obj/board/square.gd
@@ -1,9 +1,13 @@
 class_name Square
 extends Container
 
+@onready var highlight_hover: Panel = %HighlightHover
+@onready var highlight_pressed: Panel = %HighlightPressed
 
 var board: Board
 var piece: Piece
+var _hovered: bool
+var _pressed: bool
 
 
 func _enter_tree() -> void:
@@ -17,3 +21,21 @@ func _enter_tree() -> void:
             child.square = null
             piece = null
     )
+
+
+func _ready() -> void:
+    gui_input.connect(_track_pressed)
+    mouse_entered.connect(_on_hover_change.bind(true))
+    mouse_exited.connect(_on_hover_change.bind(false))
+
+
+func _track_pressed(event: InputEvent) -> void:
+    if event.is_action(&"mouse_left"):
+        _pressed = event.is_pressed()
+        highlight_pressed.visible = _pressed
+        # _pressed changed, so refresh hovered
+        _on_hover_change(_hovered)
+
+func _on_hover_change(hovered: bool) -> void:
+    _hovered = hovered
+    highlight_hover.visible = not _pressed and _hovered
--- obj/pieces/piece.gd
+++ obj/pieces/piece.gd
@@ -6,15 +6,3 @@ extends TextureRect
 @export var type: PieceData.Type
 
 var square: Square
-var _hovering: bool = false
-
-
-func _ready() -> void:
-    gui_input.connect(_on_input)
-    mouse_entered.connect(func() -> void: _hovering = true)
-    mouse_exited.connect(func() -> void: _hovering = false)
-
-
-func _on_input(event: InputEvent) -> void:
-    if event.is_action_released(&"left_click") and _hovering:
-        queue_free()
